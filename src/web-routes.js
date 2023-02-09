db87a9d257a80e3e3ea752aaf6bff4c701d59776
emanlapaz <eugeniojrmanlapaz@gmail.com>
2023-02-09 01:06:48 +0000

    Added about page

--- src/web-routes.js
+++ src/web-routes.js
@@ -1,5 +1,6 @@
 import { accountsController } from "./controllers/accounts-controller.js";
 import { dashboardController } from "./controllers/dashboard-controller.js";
+import { aboutController } from "./controllers/about-controller.js";
 
 export const webRoutes = [
   { method: "GET", path: "/", config: accountsController.index },
@@ -11,5 +12,7 @@ export const webRoutes = [
 
   { method: "GET", path: "/dashboard", config: dashboardController.index },
   { method: "POST", path: "/dashboard/addplacemark", config: dashboardController.addPlacemark },
+  { method: "GET", path: "/about", config: aboutController.index }
+
 ];
 
