6056659a22d62455399c581219aaf61e06be3245
emanlapaz <eugeniojrmanlapaz@gmail.com>
2023-02-16 02:11:36 +0000

    mongo user test pass

--- src/models/db.js
+++ src/models/db.js
@@ -1,9 +1,13 @@
 import { userMemStore } from "./mem/user-mem-store.js";
 import { placemarkMemStore } from "./mem/placemark-mem-store.js";
 import { pointMemStore } from "./mem/point-mem-store.js";
+
 import { userJsonStore } from "./json/user-json-store.js";
 import { placemarkJsonStore } from "./json/placemark-json-store.js";
 import { pointJsonStore } from "./json/point-json-store.js";
+
+import { connectMongo } from "./mongo/connect.js";
+import { userMongoStore } from "./mongo/user-mongo-store.js";
 
 export const db = {
   userStore: null,
@@ -17,6 +21,12 @@ export const db = {
         this.placemarkStore = placemarkJsonStore;
         this.pointStore = pointJsonStore;
         break;
+
+      case "mongo":
+        this.userStore = userMongoStore;
+        connectMongo();
+        break;
+        
       default:
         this.userStore = userMemStore;
         this.placemarkStore = placemarkMemStore;
