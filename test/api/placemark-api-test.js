a4b857fe8fc6c540831929e66b086281da9e3163
emanlapaz <eugeniojrmanlapaz@gmail.com>
2023-03-03 18:47:18 +0000

    placemark api completed

--- test/api/placemark-api-test.js
+++ test/api/placemark-api-test.js
@@ -23,14 +23,44 @@ suite("Placemark API tests", () => {
   teardown(async () => {});
 
   test("create placemark", async () => {
+    const returnedPlacemark = await placemarkService.createPlacemark(kildare);
+    assert.isNotNull(returnedPlacemark);
+    assertSubset(kildare, returnedPlacemark);
   });
+  
 
   test("delete a placemark", async () => {
+    const placemark = await placemarkService.createPlacemark(kildare);
+    const response = await placemarkService.deletePlacemark(placemark._id);
+    assert.equal(response.status, 204);
+    try {
+      const returnedPlacemark = await placemarkService.getPlacemark(placemark._id);
+      assert.fail("Should not return a response");
+    } catch (error) {
+      assert(error.response.data.message === "No Placemark with this id", "Incorrect Response Message");
+    }
   });
+  
 
   test("create multiple placemarks", async () => {
+    for (let i = 0; i < testPlacemarks.length; i += 1) {
+      testPlacemarks[i].userid = user._id;
+      // eslint-disable-next-line no-await-in-loop
+      await placemarkService.createPlacemark(testPlacemarks[i]);
+    }
+    let returnedPlacemarks = await placemarkService.getAllPlacemarks();
+    assert.equal(returnedPlacemarks.length, testPlacemarks.length);
+    await placemarkService.deleteAllPlacemarks();
+    returnedPlacemarks = await placemarkService.getAllPlacemarks();
+    assert.equal(returnedPlacemarks.length, 0);
   });
-
+  
   test("remove non-existant placemark", async () => {
+    try {
+      const response = await placemarkService.deletePlacemark("not an id");
+      assert.fail("Should not return a response");
+    } catch (error) {
+      assert(error.response.data.message === "No Placemark with this id", "Incorrect Response Message");
+    }
   });
 });
