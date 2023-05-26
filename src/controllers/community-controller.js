190fabd216a7a8fa3b4686456cc1cb5c76986d68
emanlapaz <eugeniojrmanlapaz@gmail.com>
2023-05-26 05:33:07 +0100

    added points to community

--- src/controllers/community-controller.js
+++ src/controllers/community-controller.js
@@ -6,10 +6,17 @@ export const communityController = {
       const loggedInUser = request.auth.credentials;
       const placemarks = await db.placemarkStore.getAllUserPlacemarks();
       console.log("Placemarks retrieved: ", placemarks);
+
+      // Get points for each placemark and add them to the placemark object
+      const placemarksWithPoints = await Promise.all(placemarks.map(async (placemark) => {
+        const points = await db.pointStore.getPointsByPlacemarkId(placemark._id);
+        return {...placemark, points};
+      }));
+
       const viewData = {
         title: "Community Dashboard",
         user: loggedInUser,
-        placemarks: placemarks,
+        placemarks: placemarksWithPoints,
       };
       return h.view("community-view", viewData);
     },
